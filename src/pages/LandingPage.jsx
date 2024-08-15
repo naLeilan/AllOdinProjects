8a774aea092d823510470d965ff6373ee2364866
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-08-15 11:12:30 +0300

    first commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,25 @@
+function LandingPage() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <h3 className="headerLogo">header logo</h3>
+        <div className="links">
+          <h3>Link1</h3>
+          <h3>Link2</h3>
+          <h3>Link3</h3>
+        </div>
+      </div>
+      <div className="main">
+        <div>
+          <h2>Nulla at tincidunt orci, at auctor augue.</h2>
+        </div>
+      </div>
+      <div className="detail1"> detail1</div>
+      <div className="detail2"> detail2</div>
+      <div className="detail3"> detail3</div>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default LandingPage;
